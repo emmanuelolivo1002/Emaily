c1bb924a439e72b5b6cec23cbcc84d36c17788e5
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-05-09 10:21:14 -0500

    Add auth Routes with passport

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+
+// Run what's in passport.js file
+require("./services/passport");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hello: "there" });
-});
+//Import auth routes and Call auth routes with our app object
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
