126063cc0cfa671f7205c7972b826be33570a56c
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-05-14 14:45:57 -0500

    Initial Client commits

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/auth/google">Sign in with Google</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
