223cd802e12325f13000e641866be02a29d29193
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-05-15 08:52:32 -0500

    Add materializecss

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 
+// Materialize css
+import "materialize-css/dist/css/materialize.min.css";
+
 import App from "./components/App";
 
 // Reducers
