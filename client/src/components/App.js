e067101fc39f131aef5f7a803c672bc03656f36c
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-05-15 09:01:17 -0500

    Add Header and preliminary content components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+// Components
+import Header from "./Header";
+
+const Dashboard = () => <h3>Dashboard</h3>;
+const SurveyNew = () => <h3>SurveyNew</h3>;
+const Landing = () => <h3>Landing</h3>;
 
 const App = () => {
   return (
-    <div>
-      <h1>Hi There</h1>
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
