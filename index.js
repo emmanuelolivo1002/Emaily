1aa7d594b23cdc8386daf7873a437e3bcac2ddd5
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-05-22 09:44:50 -0500

    Add body parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 
 // Mongoose Models
@@ -16,6 +17,9 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 const app = express();
 
 // ****** MIDDLEWARE START *******
+
+// Parse the body of an incoming request object and add it to req.body
+app.use(bodyParser.json());
 
 // Configuration for cookies with express
 app.use(
@@ -32,8 +36,13 @@ app.use(passport.session());
 
 // ****** MIDDLEWARE END *******
 
+// ****** ROUTES START *******
+
 //Import auth routes and Call auth routes with our app object
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+// ****** ROUTES END *******
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
