b670384c899564a1c952f65cbb469c79591644b4
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-05-31 11:11:55 -0500

    Add Redux Dev Tools

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reduxThunk from "redux-thunk";
 
 // Materialize css
@@ -12,8 +12,19 @@ import App from "./components/App";
 // Reducers
 import reducers from "./reducers";
 
+// TESTING
+import axios from "axios";
+window.axios = axios;
+
 // Redux store
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(
+  reducers,
+  {},
+  compose(
+    applyMiddleware(reduxThunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
